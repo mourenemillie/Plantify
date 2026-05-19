727b6555b1e64c89ed897b23cf10ffa540cc494b
mourenemillie <lifeasnanaw@gmail.com>
2026-05-19 13:24:45 +0700

    rapihin dikit yh

--- app/src/main/java/com/example/plantify/MainActivity.kt
+++ app/src/main/java/com/example/plantify/MainActivity.kt
@@ -12,7 +12,6 @@ import com.example.plantify.ui.theme.PlantifyTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             PlantifyTheme {
                 PlantifyApp()
--- app/src/main/java/com/example/plantify/ui/theme/Theme.kt
+++ app/src/main/java/com/example/plantify/ui/theme/Theme.kt
@@ -9,8 +9,12 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = PlantifyLightGreen,
@@ -52,6 +56,15 @@ fun PlantifyTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = PlantifyMediumGreen.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+        }
     }
 
     MaterialTheme(
